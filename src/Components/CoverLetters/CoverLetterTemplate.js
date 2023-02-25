ab4cd0acc729e8d96fc5e9d234ddab844d199334
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-02-25 20:45:21 +0530

    created a page where user is directed when wanted to create a cover letter, the page displays options of cover letter in mainframe component and has an object with two array one for the titles and one for the images templates

---
+++ src/Components/CoverLetters/CoverLetterTemplate.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+// importiong components
+import MainFrame from "../CommonComponents/MainFrame";
+
+//importing the rseume template images
+import coverLetter1 from "../Images/coverLetterTemplates/coverLetter1.webp";
+import coverLetter2 from "../Images/coverLetterTemplates/coverLetter2.jpg";
+import coverLetter3 from "../Images/coverLetterTemplates/coverLetter3.jpg";
+import Constants from "../Utilities/Constants/Constants";
+
+// array of images and titles for every resume template
+const coverLetterDetails = {
+  imageSrc: [coverLetter1, coverLetter2, coverLetter3],
+  imageSubTitle: ["The Rookie", "The Samurai", "Talented Perzi"],
+};
+
+function CoverLetterTemplates(){
+    return (
+      <MainFrame
+        mainFrameDetails={coverLetterDetails}
+        headerValue={Constants.headerValues.coverLetterHeader}
+        footerValue={Constants.footerValues.footer}
+      />
+    );
+}
+
+export default CoverLetterTemplates
