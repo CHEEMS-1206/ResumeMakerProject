5c564e6adafdcd9c9c87d4dffc96d736f61f8bac
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-02-25 20:51:59 +0530

    added a comment on use of react.strictMode

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // whenevr using react render in strict mode .. everything is rendered twice , to stop this from happening you can remove strict mode in index.js
   <React.StrictMode>
     <App />
   </React.StrictMode>
