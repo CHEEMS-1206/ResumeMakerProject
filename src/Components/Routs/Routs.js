2a046cee1b1f0b7c31dafb445248a454cc77fd14
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-02-25 21:33:56 +0530

    removed unlisted imports

--- src/Components/Routs/Routs.js
+++ src/Components/Routs/Routs.js
@@ -4,14 +4,9 @@ import { createBrowserRouter } from "react-router-dom";
 // importing all the components for rendering 
 import Choices from '../Selector/Choices';
 import ResumeTemplates from '../Resume/ResumeTemplates';
-import RTemplate1 from '../Resume/Templates/RTemplate1';
-import RTemplate2 from '../Resume/Templates/RTemplate2';
-import RTemplate3 from '../Resume/Templates/RTemplate3';
 import ResumePreview from "../Resume/ResumePreview";
 
 import CoverLetterTemplates from '../CoverLetters/CoverLetterTemplate';
-import CLTemplate1 from "../CoverLetters/Templates/CLTemplate1";
-import CLTemplate2 from "../CoverLetters/Templates/CLTemplate2";
 import CoverLetterPreview from "../CoverLetters/CoverLetterPreview";
 
 import AboutMe from "../AboutMe/AboutMe";
@@ -26,32 +21,12 @@ const routerName = createBrowserRouter([
     element: <ResumeTemplates />,
   },
   {
-    path: "/Resume1",
-    element: <RTemplate1 />,
-  },
-  {
-    path: "/Resume2",
-    element: <RTemplate2 />,
-  },
-  {
-    path: "/Resume3",
-    element: <RTemplate3 />,
-  },
-  {
     path: "/ResumePreview",
     element: <ResumePreview />,
   },
   {
     path: "/CoverLetterTemplates",
     element: <CoverLetterTemplates />,
-  },
-  {
-    path: "/CoverLetter1",
-    element: <CLTemplate1 />,
-  },
-  {
-    path: "/CoverLetter2",
-    element: <CLTemplate2 />,
   },
   {
     path: "/CoverLetterPreview",
--- src/Components/Utilities/ImageImports/ImageImports.js
+++ src/Components/Utilities/ImageImports/ImageImports.js
@@ -1,18 +1,11 @@
 // IMPORTED IMAGES
 import resume from "../../Images/resume.jpg";
-import myPic from "../../Images/profilePic.jpeg";
 import logo from "../../Images/logo.png";
 import coverLetter from "../../Images/coverLetter.jpg";
-import gitHubPic from "../../Images/githubPic.png";
-import profilePic from "../../Images/profilePic.jpeg";
 import preview from '../../Images/preview.webp'
 
-
 const ImageArray = {
-    profilePic : profilePic,
-    gitHubPic : gitHubPic,
     resume : resume,
-    myPic : myPic ,
     logo : logo,
     coverLetter: coverLetter,
     preview : preview
