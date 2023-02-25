abf7c5a158b242afcd261552f8d3b5f98a6f4ebb
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-02-25 20:57:56 +0530

    1. removed about me page option (commented it) 2. added comments for different pages 3. Optimised the choices page by removing contants and other imports

--- src/Components/Selector/Choices.js
+++ src/Components/Selector/Choices.js
@@ -1,83 +1,82 @@
 import React, { useState } from "react";
-import '../../StyleSheets/Choices.css'
+
+// importing styles
+import "../../StyleSheets/Choices.css";
 
 // importing useNavigate for naivigating across pages
 import { useNavigate } from "react-router-dom";
 
-// IMPORTED IMAGES
-import resume from '../Images/resume.jpg'
-import myPic from "../Images/logo.png";
-import logo from "../Images/logo.png";
-import coverLetter from "../Images/coverLetter.jpg";
+// importing Constants
+import Constants from "../Utilities/Constants/Constants";
 
-const Choices = () =>{
-  //used constants
-  const creator = "Priyanhsu Singh ";
-  // const Rights = "  All rights reserved priyanshuSingh@2023 ";
-  const resumeDefinition =
-    "A resume (or “CV” outside of the US) is a formal document that provides an overview of your professional qualifications, including your relevant work experience, skills, education, and notable accomplishments. Usually paired with a cover letter, a resume helps you demonstrate your abilities and convince employers you’re qualified and hireable.";
-  const coverLetterDefinition =
-    "A cover letter is a one-page document that you include with your resume as part of your application for a job. A good cover letter grabs a Hiring Manager’s attention and gets you to the next step of the hiring process. Cover letters are not mandatory but can help in standing out from others when applying for jobs.";
-  const aboutMe =
-    "Priyanshu Singh || Junior, B-tech ECE at MSIT || Full Stack Web developer || MERN || DSA - C++ || Expert @ Codestudio || OOP in JS, PHP, C++ || Linux Web development enthusiast, have a Solid foundational grip in front end technologies and frameworks. React developer, prefer Mysql as the database with Php as the backend language. Have made several fronted projects (can be found in my github repos).";
-  const aboutApp =
-    "Resume Maker is a handy tool that can be used to make a resume or a cover letter by just filling inthe details directly into the input fields. It is easy to use and versatile, just chose among multiple templates fill in the details and get your resume downloaded in a single click. Choose your option from the list to the right and get started.";
+// Importing Images
+import ImageArray from "../Utilities/ImageImports/ImageImports";
 
+// importing components
+import Button from "../CommonComponents/Button";
+import Header from "../CommonComponents/Header";
+import Footer from "../CommonComponents/Footer";
+
+const Choices = () => {
   let navigateToNewPath = useNavigate();
 
   // state defining
-  const [paraContent, setParaContent] = useState(aboutApp); // state of the para content
-  const [imageContent, setImageContent] = useState(logo); // state of the image
+  const [paraContent, setParaContent] = useState(Constants.aboutApp); // state of the para content
+  const [imageContent, setImageContent] = useState(ImageArray.logo); // state of the image
   const [titleContent, setTitleContent] = useState("Resume Maker"); // state of the para title
 
+  // to dynamically change the data on display on hovers
   function hoverHandler(event) {
     // change content on hover at resume button
     if (event.target.id === "RESUME") {
       setTitleContent((prevdata) => (prevdata = "Resume"));
-      setImageContent((prevdata) => (prevdata = resume));
-      setParaContent((prevdata) => (prevdata = resumeDefinition));
+      setImageContent((prevdata) => (prevdata = ImageArray.resume));
+      setParaContent((prevdata) => (prevdata = Constants.resumeDefinition));
     }
     // change content on hover at coverletter button
     else if (event.target.id === "COVERLETTER") {
       setTitleContent((prevdata) => (prevdata = "Cover Letter"));
-      setImageContent((prevdata) => (prevdata = coverLetter));
-      setParaContent((prevdata) => (prevdata = coverLetterDefinition));
+      setImageContent((prevdata) => (prevdata = ImageArray.coverLetter));
+      setParaContent(
+        (prevdata) => (prevdata = Constants.coverLetterDefinition)
+      );
     }
     // change content on hover at about me button
     else if (event.target.id === "ABOUTME") {
       setTitleContent((prevdata) => (prevdata = "About Me"));
-      setImageContent((prevdata) => (prevdata = myPic));
-      setParaContent((prevdata) => (prevdata = aboutMe));
+      setImageContent((prevdata) => (prevdata = ImageArray.myPic));
+      setParaContent((prevdata) => (prevdata = Constants.aboutMe));
     }
   }
+
   // function clickHandler to handle click for visiting pages
   // we define path depending on the button clicked and jump to that page using navigate
-  function clickHandler(event){
-    let path = ""
-    if(event.target.id === "RESUME"){
+  function clickHandler(event) {
+    let path = "";
+    if (event.target.id === "RESUME") {
       path = "ResumeTemplates";
-    }
-    else if (event.target.id === "COVERLETTER"){
+    } else if (event.target.id === "COVERLETTER") {
       path = "CoverLetterTemplates";
-    }
-    else if (event.target.id === "ABOUTME") {
+    } else if (event.target.id === "ABOUTME") {
       path = "AboutMe";
     }
-    navigateToNewPath(path)
+    navigateToNewPath(path);
   }
 
   function hoverOverHandler() {
     // change content on hover at resume maker header
     setTitleContent((prevdata) => (prevdata = "Resume Maker"));
-    setImageContent((prevdata) => (prevdata = logo));
-    setParaContent((prevdata) => (prevdata = aboutApp));
+    setImageContent((prevdata) => (prevdata = ImageArray.logo));
+    setParaContent((prevdata) => (prevdata = Constants.aboutApp));
   }
 
   return (
     <div className="chooseOptPage">
-      <div className="header" onMouseEnter={hoverOverHandler}>
-        Resume Maker
-      </div>
+      <Header
+        className="header"
+        onMouseEnter={hoverOverHandler}
+        headerValue={Constants.headerValues.choicePageHeader}
+      />
       <div className="choiceHolder">
         <div className="definitionBox">
           <h1 className="title">{titleContent}</h1>
@@ -85,35 +84,35 @@ const Choices = () =>{
           <p className="choiceDefinition">{paraContent}</p>
         </div>
         <div className="buttonHolder">
-          <button
+          <Button
             className="selectChoice"
             id="RESUME"
             onMouseEnter={hoverHandler}
             onClick={clickHandler}
-          >
-            Resume
-          </button>
-          <button
+            buttonValue={Constants.buttonValues.resume}
+          />
+          <Button
             className="selectChoice"
             id="COVERLETTER"
             onMouseEnter={hoverHandler}
             onClick={clickHandler}
-          >
-            Cover Letters
-          </button>
-          <button
+            buttonValue={Constants.buttonValues.coverLetter}
+          />
+          {/* <Button
             className="selectChoice"
             id="ABOUTME"
             onMouseEnter={hoverHandler}
             onClick={clickHandler}
-          >
-            About me
-          </button>
+            buttonValue={Constants.buttonValues.aboutMe}
+          /> */}
         </div>
       </div>
-      <div className="footer">Created by {creator}</div>
+      <Footer
+        className="footer"
+        footerValue={Constants.footerValues.footer + `${Constants.creator}`}
+      />
     </div>
   );
-}
+};
 
 export default Choices;
