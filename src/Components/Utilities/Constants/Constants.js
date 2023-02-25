4851a0f6d32e1a4af456115769276dd235d92768
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-02-25 21:03:16 +0530

    this file stores different contants within in an object to be accessed and used in multiple files

---
+++ src/Components/Utilities/Constants/Constants.js
@@ -0,0 +1,47 @@
+const Constants = {
+  //used constants in choice page
+  creator: "Priyanhsu Singh ",
+  Rights: "  All rights reserved priyanshuSingh@2023 ",
+  resumeDefinition:
+    "A resume (or “CV” outside of the US) is a formal document that provides an overview of your professional qualifications, including your relevant work experience, skills, education, and notable accomplishments. Usually paired with a cover letter, a resume helps you demonstrate your abilities and convince employers you’re qualified and hireable.",
+  coverLetterDefinition:
+    "A cover letter is a one-page document that you include with your resume as part of your application for a job. A good cover letter grabs a Hiring Manager’s attention and gets you to the next step of the hiring process. Cover letters are not mandatory but can help in standing out from others when applying for jobs.",
+  aboutMe:
+    "Priyanshu Singh || Junior, B-tech ECE at MSIT || Full Stack Web developer || MERN || DSA - C++ || Expert @ Codestudio || OOP in JS, PHP, C++ || Linux Web development enthusiast, have a Solid foundational grip in front end technologies and frameworks. React developer, prefer Mysql as the database with Php as the backend language. Have made several fronted projects (can be found in my github repos).",
+  aboutApp:
+    "Resume Maker is a handy tool that can be used to make a resume or a cover letter by just filling inthe details directly into the input fields. It is easy to use and versatile, just chose among multiple templates fill in the details and get your resume downloaded in a single click. Choose your option from the list to the right and get started.",
+
+  // values of dirrefernt buttons
+  buttonValues: {
+    resume: "Resume",
+    coverLetter: "Cover Letters",
+    aboutMe: "About Me",
+    next: "Next",
+    previous: "Previous",
+    proceed: "Proceed",
+    download: "Download",
+    back : "Return"
+  },
+
+  // values of different footers
+  footerValues: {
+    footer: "Created By ",
+  },
+
+  // values of different headers
+  headerValues: {
+    choicePageHeader: "Resume Maker",
+    resumePageHeader: "Resumes",
+    coverLetterHeader: "Cover Letters",
+    aboutMeHeader: " ",
+  },
+
+  // to store the value of selected image template
+  selectedTemplate :{
+    id : "",
+    imageSrc : "",
+    imageSubtitle : ""
+  }
+};
+
+export default Constants;
