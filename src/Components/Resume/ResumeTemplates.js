4b31a704ccca1ecb34187fcfca9b5d2f8001662a
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-02-25 20:47:06 +0530

    created a page where user is directed when wanted to create a resume, the page displays options of resumes in mainframe component and has an object with two array one for the titles and one for the images templates

---
+++ src/Components/Resume/ResumeTemplates.js
@@ -0,0 +1,26 @@
+import React from "react";
+import MainFrame from "../CommonComponents/MainFrame";
+
+//importing the rseume template images
+import resume1 from "../Images/resumeTemplates/resume1.png";
+import resume2 from "../Images/resumeTemplates/resume2.png";
+import resume3 from "../Images/resumeTemplates/resume3.jpg";
+import Constants from "../Utilities/Constants/Constants";
+
+// object for passing down as props
+const resumeDetails = {
+  imageSrc: [resume1,resume2,resume3],
+  imageSubTitle: ["The Rookie", "Curriculum Vitae", "Experienced Resume"]
+};
+
+function ResumeTemplates() {
+  return (
+    <MainFrame
+      mainFrameDetails={resumeDetails}
+      headerValue={Constants.headerValues.resumePageHeader}
+      footerValue={Constants.footerValues.footer}
+    />
+  );
+}
+
+export default ResumeTemplates;
