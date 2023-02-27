06156fd4731dfd8d5040601ad5995ca9c97c2f63
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-02-27 11:02:50 +0530

    created a formContainer component to be used to render forms

---
+++ src/Components/CommonComponents/FormContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FormContainer(props){
+    return (
+    <div className={props.className}>
+        <div></div>
+        <div></div>
+    </div>
+    );
+}
+export default FormContainer;
